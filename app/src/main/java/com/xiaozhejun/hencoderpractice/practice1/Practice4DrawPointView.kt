1ef2cd37d70d84f1b374f627071e44a0a0af8d89
LucasYang7 <731180001@qq.com>
2019-12-21 21:32:32 +0800

    练习drawPoint

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice4DrawPointView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice4DrawPointView.kt
@@ -3,6 +3,7 @@ package com.xiaozhejun.hencoderpractice.practice1
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -24,6 +25,23 @@ class Practice4DrawPointView : View {
 
         //        练习内容：使用 canvas.drawPoint() 方法画点
         //        一个圆点，一个方点
-        //        圆点和方点的切换使用 paint.setStrokeCap(cap)：`ROUND` 是圆点，`BUTT` 或 `SQUARE` 是方点
+        //        圆点和方点的切换使用 paint.setStrokeCap(cap)：`ROUND` 是圆点，`BUTT` 或 `SQUARE` 是方点f
+
+        val paint = Paint()
+        // 圆点
+        paint.strokeCap = Paint.Cap.ROUND
+        paint.strokeWidth = 200f
+        canvas.drawPoint(100f,100f,paint)
+
+        // 方点
+        paint.strokeCap = Paint.Cap.SQUARE
+        paint.strokeWidth = 200f
+        canvas.drawPoint(500f,100f,paint)
+
+        // 方点
+        paint.strokeCap = Paint.Cap.BUTT
+        paint.strokeWidth = 200f
+        canvas.drawPoint(900f,100f,paint)
+
     }
 }
