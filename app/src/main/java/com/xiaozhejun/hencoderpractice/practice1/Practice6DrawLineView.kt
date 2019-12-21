d6b21687b1235aef5a399192bf8934706afa977d
LucasYang7 <731180001@qq.com>
2019-12-21 21:49:38 +0800

    练习drawLine

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice6DrawLineView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice6DrawLineView.kt
@@ -3,8 +3,12 @@ package com.xiaozhejun.hencoderpractice.practice1
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class Practice6DrawLineView : View {
 
@@ -23,5 +27,14 @@ class Practice6DrawLineView : View {
         super.onDraw(canvas)
 
         //        练习内容：使用 canvas.drawLine() 方法画直线
+        val paint = Paint()
+        // 由于直线不是封闭图形，所以 setStyle(style) 对直线没有影响。
+        paint.strokeWidth = 20f
+        canvas.drawLine(500f, 500f, 900f, 900f, paint)
+        // 绘制一组直线
+        val points = floatArrayOf(20f, 20f, 120f, 20f, 70f, 20f, 70f, 120f, 20f, 120f, 120f, 120f,
+            150f, 20f, 250f, 20f, 150f, 20f, 150f, 120f, 250f, 20f, 250f, 120f, 150f, 120f, 250f, 120f
+        )
+        canvas.drawLines(points,paint)
     }
 }
