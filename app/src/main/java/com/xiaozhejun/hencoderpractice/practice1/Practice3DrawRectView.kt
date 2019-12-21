d502293e4b66d2bf5bcff5dd9d1b3b9d4953d534
LucasYang7 <731180001@qq.com>
2019-12-21 21:19:24 +0800

    练习drawRect

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice3DrawRectView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice3DrawRectView.kt
@@ -3,6 +3,8 @@ package com.xiaozhejun.hencoderpractice.practice1
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 
@@ -23,5 +25,16 @@ class Practice3DrawRectView : View {
         super.onDraw(canvas)
 
         //        练习内容：使用 canvas.drawRect() 方法画矩形
+        val paint = Paint()
+        // 实心矩形
+        paint.style = Paint.Style.FILL
+        canvas.drawRect(100f, 100f, 200f, 200f, paint)
+
+        // 空心矩形
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 20f
+        val rect = RectF()
+        rect.set(400f, 400f, 500f, 500f)
+        canvas.drawRect(rect, paint)
     }
 }
