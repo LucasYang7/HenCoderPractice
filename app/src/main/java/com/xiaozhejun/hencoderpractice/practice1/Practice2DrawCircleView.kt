a02dbecf989330eeb1e5f12a4d2771f35e8a6585
LucasYang7 <731180001@qq.com>
2019-12-21 21:05:50 +0800

    练习drawCircle

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice2DrawCircleView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice2DrawCircleView.kt
@@ -3,6 +3,8 @@ package com.xiaozhejun.hencoderpractice.practice1
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -24,5 +26,20 @@ class Practice2DrawCircleView : View {
 
         //        练习内容：使用 canvas.drawCircle() 方法画圆
         //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        val paint = Paint()
+        // 1.实心圆
+        canvas.drawCircle(300f, 200f, 200f, paint)
+        // 2.空心圆
+        paint.style = Paint.Style.STROKE
+        canvas.drawCircle(800f, 200f, 200f, paint)
+        // 3.蓝色实心圆
+        paint.style = Paint.Style.FILL
+        paint.color = Color.BLUE
+        canvas.drawCircle(300f, 700f, 200f, paint)
+        // 4.线宽为 20 的空心圆
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 20f
+        paint.color = Color.BLACK
+        canvas.drawCircle(800f, 700f, 200f, paint)
     }
 }
