e4f7cbc21d8ef68880a571cda8c3840859a32a4e
LucasYang7 <731180001@qq.com>
2019-12-22 16:11:02 +0800

    练习drawPath

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice9DrawPathView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice9DrawPathView.kt
@@ -2,7 +2,7 @@ package com.xiaozhejun.hencoderpractice.practice1
 
 
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
@@ -20,8 +20,18 @@ class Practice9DrawPathView : View {
     }
 
     override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
+        super.onDraw(canvas )
 
         //        练习内容：使用 canvas.drawPath() 方法画心形
+        val paint = Paint()
+        paint.color = Color.RED
+        val path = Path()
+        val rect = RectF()
+        rect.set(200f,200f,400f,400f)
+        path.addArc(rect,-225f,225f)
+        rect.set(400f,200f,600f,400f)
+        path.arcTo(rect,-180f,225f,false)
+        path.lineTo(400f,542f)
+        canvas.drawPath(path,paint)
     }
 }
