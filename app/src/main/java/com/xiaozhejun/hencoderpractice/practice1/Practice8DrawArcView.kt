e9fc27aaa664172efc68eb5c81ca6ff582f32178
LucasYang7 <731180001@qq.com>
2019-12-22 12:21:32 +0800

    练习drawArc

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice8DrawArcView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice8DrawArcView.kt
@@ -3,6 +3,8 @@ package com.xiaozhejun.hencoderpractice.practice1
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 
@@ -23,5 +25,20 @@ class Practice8DrawArcView : View {
         super.onDraw(canvas)
 
         //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        val rectF = RectF()
+        rectF.set(200f, 200f, 800f, 600f)
+        val paint = Paint()
+        // 实心扇形（useCenter为true时会带有半径）
+        paint.style = Paint.Style.FILL
+        canvas.drawArc(rectF, 0f, 45f, true, paint)
+        // 空心扇形
+        paint.style = Paint.Style.STROKE
+        canvas.drawArc(rectF, 90f, 45f, true, paint)
+        // 实心圆弧
+        paint.style = Paint.Style.FILL
+        canvas.drawArc(rectF, 180f, 45f, false, paint)
+        // 空心圆弧
+        paint.style = Paint.Style.STROKE
+        canvas.drawArc(rectF, 270f, 45f, false, paint)
     }
 }
