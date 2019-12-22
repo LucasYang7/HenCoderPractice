feb57dbf51852f8832d56cbc9f7a2052fe8f4712
LucasYang7 <731180001@qq.com>
2019-12-22 22:10:21 +0800

    练习绘制直方图

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice10HistogramView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice1/Practice10HistogramView.kt
@@ -3,6 +3,9 @@ package com.xiaozhejun.hencoderpractice.practice1
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 
@@ -24,5 +27,39 @@ class Practice10HistogramView : View {
 
         //        综合练习
         //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        val textPaint = Paint()
+        textPaint.color = Color.WHITE
+        textPaint.style = Paint.Style.FILL
+        textPaint.textSize = 30f
+        val paint = Paint()
+        paint.color = Color.WHITE
+        canvas.drawLine(0f,100f,0f,600f,paint)
+        canvas.drawLine(0f,600f,1000f,600f,paint)
+        paint.color = Color.GREEN
+        paint.style = Paint.Style.FILL
+        val rect = RectF()
+        rect.set(100f, 500f, 200f, 600f)
+        canvas.drawRect(rect, paint)
+        canvas.drawText("Froyo", 100f, 650f, textPaint)
+
+        rect.set(250f, 300f, 350f, 600f)
+        canvas.drawRect(rect, paint)
+        canvas.drawText("GB", 250f, 650f, textPaint)
+
+        rect.set(400f, 200f, 500f, 600f)
+        canvas.drawRect(rect, paint)
+        canvas.drawText("ICS", 400f, 650f, textPaint)
+
+        rect.set(550f, 300f, 650f, 600f)
+        canvas.drawRect(rect, paint)
+        canvas.drawText("JB", 550f, 650f, textPaint)
+
+        rect.set(700f, 200f, 800f, 600f)
+        canvas.drawRect(rect, paint)
+        canvas.drawText("KitKat", 700f, 650f, textPaint)
+
+        rect.set(850f, 500f, 950f, 600f)
+        canvas.drawRect(rect, paint)
+        canvas.drawText("L", 850f, 650f, textPaint)
     }
 }
