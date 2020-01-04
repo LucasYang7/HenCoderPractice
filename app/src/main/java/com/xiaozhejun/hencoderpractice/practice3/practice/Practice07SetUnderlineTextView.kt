d2b622464cd70e7d9938a5da1778b9fbbe3cafc1
LucasYang7 <731180001@qq.com>
2020-01-04 15:43:32 +0800

    练习setUnderlineTextView

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice3/practice/Practice07SetUnderlineTextView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice3/practice/Practice07SetUnderlineTextView.kt
@@ -6,25 +6,18 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
-class Practice07SetUnderlineTextView : View {
+class Practice07SetUnderlineTextView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet?,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
     internal var paint = Paint(Paint.ANTI_ALIAS_FLAG)
     internal var text = "Hello HenCoder"
-
-    constructor(context: Context) : super(context) {}
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    ) {
-    }
 
     init {
         paint.textSize = 60f
 
         // 使用 Paint.setUnderlineText() 来设置下划线
+        paint.isUnderlineText = true
     }
 
     override fun onDraw(canvas: Canvas) {
