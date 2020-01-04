6b2dd1c9430c4e2c0415fd49cc8ce63a81bc5c4c
LucasYang7 <731180001@qq.com>
2020-01-04 16:11:43 +0800

    练习setTextScaleX

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice3/practice/Practice09SetTextScaleXView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice3/practice/Practice09SetTextScaleXView.kt
@@ -6,30 +6,28 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
-class Practice09SetTextScaleXView : View {
+class Practice09SetTextScaleXView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
     internal var paint = Paint(Paint.ANTI_ALIAS_FLAG)
     internal var text = "Hello HenCoder"
 
-    constructor(context: Context) : super(context) {}
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    ) {
-    }
-
     init {
         paint.textSize = 60f
-
-        // 使用 Paint.setTextScaleX() 来改变文字宽度
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
+        // 使用 Paint.setTextScaleX() 来改变文字宽度
+        paint.textScaleX = 1.0f
         canvas.drawText(text, 50f, 100f, paint)
+
+        paint.textScaleX = 0.5f
+        canvas.drawText(text, 50f, 200f, paint)
+
+        paint.textScaleX = 1.2f
+        canvas.drawText(text, 50f, 300f, paint)
     }
 }
