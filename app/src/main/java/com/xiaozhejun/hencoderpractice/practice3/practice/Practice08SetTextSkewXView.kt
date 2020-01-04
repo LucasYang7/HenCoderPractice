fb5af9c2f5b7d5db3d903ec4deb83b4d5e5303d6
LucasYang7 <731180001@qq.com>
2020-01-04 15:51:29 +0800

    练习setTextSkewXView

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice3/practice/Practice08SetTextSkewXView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice3/practice/Practice08SetTextSkewXView.kt
@@ -6,25 +6,19 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
-class Practice08SetTextSkewXView : View {
+class Practice08SetTextSkewXView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
     internal var paint = Paint(Paint.ANTI_ALIAS_FLAG)
     internal var text = "Hello HenCoder"
-
-    constructor(context: Context) : super(context) {}
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    ) {
-    }
 
     init {
         paint.textSize = 60f
 
         // 使用 Paint.setTextSkewX() 来让文字倾斜
+        paint.textSkewX = -0.5f
     }
 
     override fun onDraw(canvas: Canvas) {
