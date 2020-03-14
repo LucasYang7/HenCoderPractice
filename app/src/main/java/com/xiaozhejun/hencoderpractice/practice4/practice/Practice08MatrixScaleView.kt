e4a5cb4707407892ab1895961a494f7019a751b6
LucasYang7 <731180001@qq.com>
2020-03-14 23:05:05 +0800

    练习Matrix scale

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice4/practice/Practice08MatrixScaleView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice4/practice/Practice08MatrixScaleView.kt
@@ -1,11 +1,7 @@
 package com.xiaozhejun.hencoderpractice.practice4.practice
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Point
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import com.xiaozhejun.hencoderpractice.R
@@ -20,6 +16,7 @@ class Practice08MatrixScaleView @JvmOverloads constructor(
     internal var bitmap: Bitmap
     internal var point1 = Point(200, 200)
     internal var point2 = Point(600, 200)
+    internal var matrix = Matrix()
 
 
     init {
@@ -28,8 +25,30 @@ class Practice08MatrixScaleView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
+        matrix.reset()
+        matrix.preScale(
+            1.5f,
+            1.5f,
+            point1.x.toFloat() + bitmap.width / 2,
+            point1.y.toFloat() + bitmap.height / 2
+        )
+        canvas.save()
+        canvas.concat(matrix)
         canvas.drawBitmap(bitmap, point1.x.toFloat(), point1.y.toFloat(), paint)
+        canvas.restore()
+
+        // 以(point2.x.toFloat() + bitmap.width / 2,point2.y.toFloat() + bitmap.height / 2)为中心点，
+        // 进行缩放操作
+        matrix.reset()
+        matrix.preScale(
+            0.5f,
+            2.0f,
+            point2.x.toFloat() + bitmap.width / 2,
+            point2.y.toFloat() + bitmap.height / 2
+        )
+        canvas.save()
+        canvas.concat(matrix)
         canvas.drawBitmap(bitmap, point2.x.toFloat(), point2.y.toFloat(), paint)
+        canvas.restore()
     }
 }
