0640d79d9d38a158548643579567dccc343f2877
LucasYang7 <731180001@qq.com>
2020-03-14 22:39:23 +0800

    练习matrix translate

--- app/src/main/java/com/xiaozhejun/hencoderpractice/practice4/practice/Practice07MatrixTranslateView.kt
+++ app/src/main/java/com/xiaozhejun/hencoderpractice/practice4/practice/Practice07MatrixTranslateView.kt
@@ -1,11 +1,7 @@
 package com.xiaozhejun.hencoderpractice.practice4.practice
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Point
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import com.xiaozhejun.hencoderpractice.R
@@ -20,7 +16,7 @@ class Practice07MatrixTranslateView @JvmOverloads constructor(
     internal var bitmap: Bitmap
     internal var point1 = Point(200, 200)
     internal var point2 = Point(600, 200)
-
+    internal var matrix = Matrix()
     init {
         bitmap = BitmapFactory.decodeResource(resources, R.drawable.maps)
     }
@@ -28,7 +24,20 @@ class Practice07MatrixTranslateView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        // 将画布向左上角移动
+        matrix.reset()
+        matrix.postTranslate(-100f,-100f)
+        canvas.save()
+        canvas.concat(matrix)
         canvas.drawBitmap(bitmap, point1.x.toFloat(), point1.y.toFloat(), paint)
+        canvas.restore()
+
+        // 将画布向右边平移
+        matrix.reset()
+        matrix.postTranslate(200f,0f)
+        canvas.save()
+        canvas.concat(matrix)
         canvas.drawBitmap(bitmap, point2.x.toFloat(), point2.y.toFloat(), paint)
+        canvas.restore()
     }
 }
